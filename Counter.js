ddf93f98427bd79de4b9b00575931edb5c8fff6f
svmihar <svmihar@gmail.com>
2019-04-06 13:11:49 +0700

    final version, with video and ending remark

--- Counter.js
+++ Counter.js
@@ -2,61 +2,52 @@ import React from 'react'
 import styled from '@emotion/styled'
 import { space, color } from 'styled-system'
 import Typist from 'react-typist'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCode, faBeer } from "@fortawesome/free-solid-svg-icons";
 
 const Root = styled.div({
   display: 'flex',
-  alignItems: 'center',
+  justifyContent:'left'
 })
 
-const Button = styled.button(
-  {
-    appearance: 'none',
-    fontFamily: 'inherit',
-    fontSize: 'inherit',
-    fontWeight: 'bold',
-    borderRadius: '4px',
-    border: 'none',
-    width: '2em',
-    '&:focus': {
-      outline: 'none',
-      boxShadow: '0 0 0 2px magenta',
-    },
-  },
-  space,
-  color
-)
-Button.defaultProps = {
-  m: 0,
-  px: 3,
-  py: 2,
-  color: 'background',
-  bg: 'text',
-}
-
-const Samp = styled.samp(space)
+library.add(faCode, faBeer);
 
 export default class Counter extends React.Component {
-  state = {
-    count: 0,
-  }
 
-  inc = () => {
-    this.setState(state => ({ count: state.count + 1 }))
-  }
-
-  dec = () => {
-    this.setState(state => ({ count: state.count - 1 }))
-  }
-
-  render() {
+  render() { 
     return (
       <Root>
         <Typist>
-          please animate this text
-          <Typist.Backspace count={24}/>
-          this is the second part. 
+          {/* saya tian
+          <Typist.Delay ms={3250} />
+          <Typist.Backspace count={9}/>
+          i like nirvana
+          <Typist.Delay ms={3250} />
+          <Typist.Backspace count={14}/>
+          adobe family is my poison
+          <Typist.Delay ms={3250} />
           <Typist.Backspace count={25}/>
-          masih gak tau cara ngeloop dari awal
+          i don't really make videos anymore 
+          <Typist.Delay ms={3250} />
+          <Typist.Backspace count={34}/>
+          tinggal di lab ilkom/ropd
+          <Typist.Delay ms={3250} />
+          <Typist.Backspace count={25}/>
+          i code python and javascript
+          <Typist.Delay ms={3250} />
+          <Typist.Backspace count={28}/>
+          i love to eat tahu tek 
+          <Typist.Delay ms={3250} />
+          <Typist.Backspace count={24}/> */}
+          <FontAwesomeIcon icon="beer" /> 
+          <br/>
+          visit me at
+          <br/>
+          github.com/svmihar/
+          <br/>
+          @svmihar 
+          <Typist.Delay ms={3250} />
         </Typist>
       </Root>
     )
